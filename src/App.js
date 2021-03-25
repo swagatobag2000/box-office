2e3d92153b6de96a9929899c255867a47be4cc96
swagatobag2000 <1805537@kiit.ac.in>
2021-03-25 20:35:57 +0530

    Installed Router and Switch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Route, Switch } from "react-router";
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is home page
+      </Route>
+
+      <Route exact path="/starred">
+        This is starred page
+      </Route>
+
+      <Route >
+        This is 404 page
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
