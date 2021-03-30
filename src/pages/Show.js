83392ee79217843e55c0bde6bf252e6dca66a0ea
swagatobag2000 <1805537@kiit.ac.in>
2021-03-30 09:34:26 +0530

    added loading and error message to the show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -3,15 +3,36 @@ import { useParams } from 'react-router-dom';
 import { APIGet } from '../misc/config';
 const Show = () => {
   const { id } = useParams();
-  console.log(id);
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
-    APIGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-      setShow(results);
-    });
+    let isMounted = true;
+    APIGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          setShow(results);
+          setIsLoading(false);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
 
-  console.log(show);
+  if (isLoading) {
+    return <div>Data is being loaded</div>;
+  }
+  if (error) {
+    return <div>Error Occured : {error}</div>;
+  }
   return <div>This is show page</div>;
 };
 
