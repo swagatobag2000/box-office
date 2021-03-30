f1c18cdc435d6dec3a9d824ad5285d1288e04527
swagatobag2000 <1805537@kiit.ac.in>
2021-03-30 13:48:32 +0530

    replace useState with useReducer on the show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,39 +1,67 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
-import { APIGet } from '../misc/config';
+import { apiGet } from '../misc/config';
+
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'FETCH_SUCCESS': {
+      return { isLoading: false, error: null, show: action.show };
+    }
+
+    case 'FETCH_FAILED': {
+      return { ...prevState, isLoading: false, error: action.error };
+    }
+
+    default:
+      return prevState;
+  }
+};
+
+const initialState = {
+  show: null,
+  isLoading: true,
+  error: null,
+};
+
 const Show = () => {
   const { id } = useParams();
-  const [show, setShow] = useState(null);
-  const [isLoading, setIsLoading] = useState(null);
-  const [error, setError] = useState(null);
+
+  const [{ show, isLoading, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   useEffect(() => {
     let isMounted = true;
-    APIGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
       .then(results => {
         if (isMounted) {
-          setShow(results);
-          setIsLoading(false);
+          dispatch({ type: 'FETCH_SUCCESS', show: results });
         }
       })
       .catch(err => {
         if (isMounted) {
-          setError(err.message);
-          setIsLoading(false);
+          dispatch({ type: 'FETCH_FAILED', error: err.message });
         }
       });
+
     return () => {
       isMounted = false;
     };
   }, [id]);
 
+  console.log('show', show);
+
   if (isLoading) {
     return <div>Data is being loaded</div>;
   }
+
   if (error) {
-    return <div>Error Occured : {error}</div>;
+    return <div>Error occured: {error}</div>;
   }
-  return <div>This is show page</div>;
+
+  return <div>this is show page</div>;
 };
 
 export default Show;
